9a260270fbd4c91ed5f0d0f7c2fdef5e7fb273eb
Oleh-PO <oleh.po.2007@gmail.com>
2022-04-18 20:41:09 +0300

    update inventory part 1

--- code2.js
+++ code2.js
@@ -2,13 +2,26 @@ var map = document.getElementById("map")
 var ctx = map.getContext("2d");
 var block = 50;
 var tails = 19;
+var playerIron = 0;
+var playerCopper = 0;
 var inventoryX = 500;
 var inventoryY = 350;
 var ironOreNumber = 5;
 var yOffsets = 0;
 var gamePause = false;
 var inventoryOpen = false;
-var random75 = function() {
+var inventoryMap = {};
+var p = 0;
+var circle = function(color, x, y, radius) {
+	ctx.beginPath();
+	ctx.strokeStyle = color;
+	ctx.fillStyle = color;
+	ctx.arc(x, y, radius, 0, Math.PI*2, true);
+	ctx.fill();
+	ctx.stroke();
+};
+// circle("#b87333", 100, 100, block/2);
+var random75 = function() { //3/4 random chands
 	var tree = Math.floor(Math.random()*4+1);
 	if (tree === 4) {
 		return false;
@@ -16,7 +29,7 @@ var random75 = function() {
 		return true;
 	};
 };
-var random25 = function() {
+var random25 = function() { //1/4 random chands
 	var one = Math.floor(Math.random()*4+1);
 	if (one === 1) {
 		return true;
@@ -31,25 +44,52 @@ var keys = {
 	87 : "W",
 	69 : "E",
 }
+var inventoryPlayer = [];
 var player = {
 	color : "DarkRed",
-	x : 0,
-	y : 0,
+	x : 950,
+	y : 500,
 };
-$("body").keydown(function(event) {
+player.inventory = {
+	iron : 0,
+	copper : 0,
+};
+$("#map").click(function(event) { //click detector
+	playerIron = 0;
+	playerCopper = 0;
+	var clickX = Math.floor(event.pageX/block);
+	var clickY = Math.floor(event.pageY/block);
+	if (gamePause === false && inventoryOpen === false) {	
+		if (Math.abs(clickX - player.x / block) <= 3 && Math.abs(clickY - player.y / block) <= 3) {
+			inventoryPlayer.push((blockMapX[clickX])[clickY]);
+			for (var i = 0; i < inventoryPlayer.length; i++) {
+				if (inventoryPlayer[i] === "iron") {
+					playerIron++;
+				} else if (inventoryPlayer[i] === "copper") {
+					playerCopper++;
+				};
+			};
+			player.inventory = {
+				iron : playerIron,
+				copper : playerCopper,
+			};
+		};
+	};
+});
+$("body").keydown(function(event) { //keybord detector
 	if (gamePause === false) {
 		switch(event.keyCode) {
 			case 68: 
-				player.x = player.x+ 5;
+				player.x = player.x+ 10;
 				break;
 			case 83:
-				player.y =  player.y + 5;
+				player.y =  player.y + 10;
 				break;
 			case 65:
-				player.x =  player.x- 5;
+				player.x =  player.x- 10;
 				break;
 			case 87:
-				player.y =  player.y- 5;
+				player.y =  player.y- 10;
 				break;
 			case 32:
 				if (inventoryOpen === false) {
@@ -68,7 +108,7 @@ $("body").keydown(function(event) {
 	}
 });
 var blockMapX = {};
-var tailsRandom = function() {
+var tailsRandom = function() { //random map generator
 	var r = Math.floor(Math.random()*10);
 	if (r >= 0 && r <= 3) {
 		return "grass";
@@ -80,10 +120,12 @@ var tailsRandom = function() {
 		return "grass";
 	};
 };
-var inventory = function() {
+var inventory = function() { //inventory criator
+	var inventoryMap = {};
 	var xOffsets = 0;
 	var yOffsets = 0;
 	ctx.fillStyle = "LightGray";
+	ctx.strokeStyle = "Black";
 	ctx.fillRect(inventoryX, inventoryY, 900, 250);
 	ctx.lineWidth = 5;
 	ctx.strokeRect(inventoryX, inventoryY, 900, 250);
@@ -94,6 +136,37 @@ var inventory = function() {
 			yOffsets++;
 			xOffsets = 0;
 		};
+	};
+	if (inventoryPlayer.length > 0) {
+		if (player.inventory.iron > 0 || player.inventory.copper > 0) {
+			for (var i = 0; inventoryMap[i] !== undefined; i++) {};
+			inventoryMap[i] = ["iron" , player.inventory["iron"]];
+			inventoryMap[i+1] = ["copper" , player.inventory["copper"]];
+		};
+	};
+	for (var i = 0; i < 50; i++) {
+		var o = 0;
+		if (o === 9) {
+			p ++;
+			o = 0;
+		}
+		if (inventoryMap[i][0] === "iron" && inventoryMap[i][1] !== 0) {
+			circle("#61666A", inventoryX + block/2 + block*i, inventoryY + block/2, block/3);
+			ctx.fillStyle = "Black";
+			ctx.font = "25px Arial";
+			ctx.fillText(inventoryMap[i][1], inventoryX + block/1.90 + block*i,  inventoryY + block);
+		} else if (inventoryMap[i][0] === "copper" && inventoryMap[i][1] !== 0) {
+			circle("#b87333", inventoryX + block/2 + block*i, inventoryY + block/2, block/3);
+			ctx.fillStyle = "Black";
+			ctx.font = "25px Arial";
+			ctx.fillText(inventoryMap[i][1], inventoryX + block/1.90 + block*i,  inventoryY + block);
+		} else if (inventoryMap[i][1] !== 0) {
+			circle("Black", inventoryX + block/2 + block*i, inventoryY + block/2, block/3);
+			ctx.fillStyle = "Black";
+			ctx.font = "25px Arial";
+			ctx.fillText(inventoryMap[i][1], inventoryX + block/1.90 + block*i,  inventoryY + block);
+		};
+		o++;
 	};
 };
 var mapGenerator =  function() {
